26d98cbe7a127e109c48d5ef2f83605c4f1c0fe8
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2017-12-21 14:15:03 -0800

    Hello world package

---
+++ packages/fired-up-hello-world/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FiredUpHelloWorld extends Component {
+    render() {
+        return (
+            <div>
+                Hello, World.
+            </div>
+        )
+    }
+}
+
+export default FiredUpHelloWorld;
